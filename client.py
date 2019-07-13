5e300891fef9f59b9242619b6d37257c2c166591
leytpapas <leytpapas@hotmail.com>
2019-07-13 23:22:28 +0300

    Refractored code/updated to work with python 3.7

--- client.py
+++ client.py
@@ -1,178 +1,234 @@
-import socket   #for sockets
-import sys  #for exit
+import socket
+import sys
 import os
 import time
-from thread import *
-flist=[]
+import threading
 
-def get_open_port():
-	w = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	w.bind(("",0))
-	w.listen(1)
-	port = w.getsockname()[1]
-	w.close()
-	return port
 
-def rcvFile(name,port,s):
-	
-	HOST = ''   # Symbolic name meaning all available interfaces
-	PORT = port # Arbitrary non-privileged port
-	
-	z = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	#print 'Socket created'
-	
-	#Bind socket to local host and port
-	try:
-		z.bind((HOST, PORT))
-	except socket.error , msg:
-		print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-		sys.exit()
-	s.sendall('OK')
-	s.sendall('|')
-	#print 'Socket bind complete'
+class Client:
+	def __init__(self):
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.files = [f for f in os.listdir('.') if (os.path.isfile(f) and f != sys.argv[0])]
+		self.listen = threading.Thread(target=self.listenThread, args=(self.s,))
+		self.end = False
 
-	#Start listening on socket
-	z.listen(1)
-	#print 'Socket now listening'
 
-	sc, address = z.accept()
-	print 'Receiving file: ' + name + ' from ' + address[0] + ':' + str(address[1])
-	
-	f = open(name,'wb') #open in binary
+	def get_open_port(self):
+		w = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		w.bind(("", 0))
+		w.listen(1)
+		port = w.getsockname()[1]
+		w.close()
+		return port
 
-	l = sc.recv(1024)
-	while (l):
-		f.write(l)
-		l = sc.recv(1024)
-	f.close()
-	sc.close()
-	
-	z.close()
-	
-	print 'File received'
+	def rcvFile(self, port):
 
-def listenThread(s):
-	#infinite loop so that function do not terminate and thread do not end.
-	while True:
-		choice=""
-		data=""
-		while data!='|':
-			data=s.recv(1)
-			if data=='|':
-				break
-			choice=choice+data
-			
-		#data = s.recv(1024)
-		if choice=='OK':
-			port=get_open_port()
-			start_new_thread(rcvFile,(message,port,s))
-			s.sendall(str(port))
-			s.sendall('|')
-		elif choice=='send':
-			host=""
-			data=""
-			port=""
-			name=""
-			while data!='|':
-				data=s.recv(1)
-				if data=='|':
-					break
-				host=host+data
-			data=""
-			while data!='|':
-				data=s.recv(1)
-				if data=='|':
-					break
-				port=port+data
-			port=int(port)
-			data=""
-			while data!='|':
-				data=s.recv(1)
-				if data=='|':
-					break
-				name=name+data
-			
-			data=""
-			while data!='|':
-				data=s.recv(1)
-				if data=='|':
-					break
-				binding=binding+data
-			if binding=='OK':
+
+		z = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# print 'Socket created'
+
+		# Bind socket to local host and port
+		try:
+			z.bind(('', port))
+		except socket.error as msg:
+			print('Bind failed. Error : ' + str(msg))
+			sys.exit()
+		# self.s.sendall(('OK'+'|').encode('utf-8'))
+		# print 'Socket bind complete'
+
+		# Start listening on socket
+		z.listen(1)
+		# print 'Socket now listening'
+
+		sc, address = z.accept()
+		name = ""
+		data = ""
+		while data != '|' and not self.end:
+			try:
+				data = z.recv(1).decode('utf-8')
+			except socket.timeout as e:
+				err = e.args[0]
+				# this next if/else is a bit redundant, but illustrates how the
+				# timeout exception is setup
+				if err == 'timed out':
+					time.sleep(1)
+					#print('recv timed out, retry later')
+					continue
+				else:
+					print(e)
+					sys.exit(1)
+			except socket.error as e:
+				# Something else happened, handle error, exit, etc.
+				print(e)
+				self.end = True
+				sys.exit(1)
+			else:
+				if data == '|':
+						break
+				else:
+					name += data
+		print('Receiving file: ' + name + ' from ' + address[0] + ':' + str(address[1]))
+
+		f = open(name, 'wb')  # open in binary
+
+		l = sc.recv(1024).decode('utf-8')
+		while l:
+			f.write(l)
+			l = sc.recv(1024).decode('utf-8')
+		f.close()
+		sc.close()
+
+		z.close()
+		self.s.sendall(('OK' + '|').encode('utf-8'))
+		self.files.append(name)
+		print('File received')
+
+
+	def listenThread(self, s):
+		while not self.end:
+			choice = ""
+			data = ""
+			while data != '|':
+				try:
+					data = s.recv(1).decode('utf-8')
+				except socket.timeout as e:
+					err = e.args[0]
+					# this next if/else is a bit redundant, but illustrates how the
+					# timeout exception is setup
+					if err == 'timed out':
+						time.sleep(1)
+						# print('recv timed out, retry later')
+						continue
+					else:
+						print(e)
+						sys.exit(1)
+				except socket.error as e:
+					# Something else happened, handle error, exit, etc.
+					print(e)
+					self.end = True
+					sys.exit(1)
+				else:
+					if data == '|':
+						break
+					else:
+						choice += data
+
+			# data = s.recv(1024)
+
+			if choice == 'OK':
+				port = self.get_open_port()
+				t = threading.Thread(target=self.rcvFile, args=port)
+				t.start()
+				s.sendall((str(port)+'|').encode('utf-8'))
+
+			elif choice == 'send':
+				host = ""
+				data = ""
+				port = ""
+				name = ""
+				while data != '|':
+					data = s.recv(1).decode('utf-8')
+					if data == '|':
+						break
+					host += data
+				print('Host', host)
+				data = ""
+				while data != '|':
+					data = s.recv(1).decode('utf-8')
+					if data == '|':
+						break
+					port += data
+				print('Port', port)
+				port = int(port)
+				data = ""
+				while data != '|':
+					data = s.recv(1).decode('utf-8')
+					if data == '|':
+						break
+					name += data
+				print('name',name)
+				# data = ""
+				# binding = ""
+				# while data != '|':
+				# 	data = s.recv(1).decode('utf-8')
+				# 	if data == '|':
+				# 		break
+				# 	binding += data
+				# print("binding", binding)
+
+				# if binding == 'OK':
 				z = socket.socket()
-				z.connect((host,port))
-				f=open (name, "rb") 
+				z.connect((host, port))
+				f = open(name, "rb")
 				l = f.read(1024)
-				while (l):
-					z.send(l)
+				z.sendall((name+'|').encode('utf-8'))
+				while l:
+					z.sendall(l.encode('utf-8'))
 					l = f.read(1024)
 				z.close()
-				print 'Sending completed'
-		else:
-			print choice
+				print('Sending completed')
+			else:
+				print(choice)
+
+	def sendMessage(self, s, message):
+		try:
+			# Set the whole string
+			s.sendall(message.encode('utf-8'))
+		except s.error:
+			# Send failed
+			print('Send failed')
+			sys.exit()
+		# print 'Message send successfully'
+
+	def run(self):
+		host = 'localhost'  # input('Enter host to connect: ')  # '192.168.1.9'
+		port = int(input('Enter port: '))  # 8888
+
+		try:
+			remote_ip = socket.gethostbyname(host)
+		except socket.gaierror:
+			# could not resolve
+			print('Hostname could not be resolved. Exiting')
+			self.s.close()
+			sys.exit()
+
+		# print 'Ip address of ' + host + ' is ' + remote_ip
+
+		# Connect to remote server
+		self.s.connect((remote_ip, port))
+		self.s.settimeout(2)
+		self.listen.start()
+
+		print('Client Connected to ' + host) # + ' on ip ' + remote_ip)
+
+		while True:
+			time.sleep(1)
+			message = input("Enter:\n\t'up' for uploading files\n\t'down' for downloading the file list\n\t'ask' for getting the desired file\n\t'list' for list of files\n\t'exit' for exit\n")
+
+			self.sendMessage(self.s, message + '|')
+
+			if message == 'exit':
+				try:
+					self.s.close()
+				except Exception as err:
+					pass
+				break
+			elif message == 'up':
+				self.files = [f for f in os.listdir('.') if (os.path.isfile(f) and f != sys.argv[0])]
+				for i in self.files:
+					# print "Sending ",i
+					self.sendMessage(self.s, i + '>')
+
+				self.sendMessage(self.s, '|')
+			elif message == 'ask':
+				message = input("Enter name of file:")
+				print("Asking for file: ", message)
+				self.sendMessage(self.s, message + '|')
+			elif message == 'list':
+				print('This Client has the following files: \n')
+				for i in self.files:
+					print(i + ' ')
 
 
-def sendMessage(socket,message):
-	try:
-		#Set the whole string
-		s.sendall(message)
-	except socket.error:
-		#Send failed
-		print 'Send failed'
-		sys.exit()
-	#print 'Message send successfully'
-
-try:
-    #create an AF_INET, STREAM socket (TCP)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except socket.error, msg:
-    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
-    sys.exit();
- 
-print 'Socket Created'
- 
-host = raw_input('Enter host to connect: ') #'192.168.1.9'
-port = int(raw_input('Enter port: ')) #8888
-
-try:
-    remote_ip = socket.gethostbyname( host )
-    
-except socket.gaierror:
-    #could not resolve
-    print 'Hostname could not be resolved. Exiting'
-    sys.exit()
-    
-#print 'Ip address of ' + host + ' is ' + remote_ip
-
-#Connect to remote server
-s.connect((remote_ip , port))
-start_new_thread(listenThread ,(s,))
-
-print 'Client Connected to ' + host #+ ' on ip ' + remote_ip
-
-while True:
-	time.sleep(1)
-	message = raw_input("Enter:\n\t'up' for uploading files\n\t'down' for downloading the file list\n\t'ask' for getting the desired file\n\t'exit' for exit\n")
-	
-	sendMessage(s,message)
-	sendMessage(s,'|')
-	
-	if message=='exit':
-		s.close()
-		break
-	elif message=='up':
-		files = [f for f in os.listdir('.') if (os.path.isfile(f) and f!=sys.argv[0])]
-		for i in files:
-			#print "Sending ",i
-			sendMessage(s,i)
-			sendMessage(s,'>')
-		sendMessage(s,'|')
-	elif message=='ask':
-		message = raw_input("Enter name of file:")
-		print "Asking for file: ",message
-		sendMessage(s,message)
-		sendMessage(s,'|')
-	elif message=='list':
-		print 'This Client has the following files: \n'
-		for i in files:
-			print i + ' '
+client = Client()
+client.run()
--- server.py
+++ server.py
@@ -1,188 +1,182 @@
 import socket
 import sys
-from thread import *
-clist=[]
-alist=[]
-flist=[]
-
-HOST = ''   # Symbolic name meaning all available interfaces
-PORT = 8888 # Arbitrary non-privileged port
- 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Socket created'
- 
-#Bind socket to local host and port
-try:
-    s.bind((HOST, PORT))
-except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
-     
-print 'Socket bind complete'
-
-#Start listening on socket
-s.listen(10)
-print 'Socket now listening'
+import threading
 
 
-#Function for handling connections. This will be used to create threads
-def clientthread(conn,addr):
-	#Sending message to connected client
-	conn.send('Welcome to the server.\n') #send only takes string
-	conn.send('|')
-	#infinite loop so that function do not terminate and thread do not end.
-	while True:
-		
-		#Receiving from client
-		choice=""
-		data=""
-		while data!='|':
-			data=conn.recv(1)
-			if data=='|':
-				break
-			choice=choice+data
-		reply = addr[0] + ':' + str(addr[1]) + ' sent: ' + choice + '\n'
-		print reply
-		
-		if choice == 'exit':
-			s=0
-			for a in clist:
-				if a == conn:
-					reply = addr[0] + ':' + str(addr[1]) + ' disconnected from server.\n|'
-					for i in clist:
-						if i == conn:
-							#flist[s]=[]
-							continue
-						i.sendall(reply)
-					clist.remove(a)
-					flist[s]=[]
-					flist.remove([])
-					break
-					
-				s=s+1
-			print reply
-			break
-		
-		#uploading list of files for each client
-		elif choice == 'up':
-			s=0
-			for i in clist:
-				if i == conn:
-					
-					flist[s]=[]
-					word=""
-					#print 'Ok'
-					while word!='|':
-						data=""
-						word=""
-						while word!='>':
-							#print 'OK3'
-							word=conn.recv(1)
-							if word=='|':
-								break
-							elif word=='>':
-								flist[s].append(data)
-								break
-							data=data+word
-					print 'File list has been updated:\n'
-					print  flist
-					break
-				s=s+1
-			continue
-		#downloading list of files
-		elif choice == 'down':
-			reply= '\nFiles list:'
-			s=0
-			for i in clist:
-				reply=reply + '\nClient' + str(s+1) + ': '
-				for a in range(len(flist[s])):
-					reply=reply + flist[s][a] + ' '
-				s=s+1
-			conn.sendall(reply)
-			conn.sendall('|')
-			continue
-		elif choice == 'ask':
-			choice=""
-			data=""
-			while data!='|':
-				data=conn.recv(1)
-				if data=='|':
-					break
-				choice=choice+data
-			print 'Searching for file ' + choice 
-			found=0
-			s=0
-			for i in clist:
-				if i == conn:
-					s=s+1
-					continue
-				for a in range(len(flist[s])):
-					if choice==flist[s][a]:
-						reply='OK'
-						conn.sendall(reply)
-						conn.sendall('|')
-						data=""
-						port=""
-						while data!='|':
-							data=conn.recv(1)
-							if data=='|':
-								break
-							port=port+data
-						reply='send'
-						i.sendall(reply)
-						i.sendall('|')
-						reply=addr[0]
-						i.sendall(reply)
-						i.sendall('|')
-						reply=port
-						i.sendall(reply)
-						i.sendall('|')
-						
-						reply=choice
-						i.sendall(reply)
-						i.sendall('|')
-						
-						found=1
+class Server:
+
+	def __init__(self, host='', port=8888, max_clients=10):
+		self.host = host
+		self.port = port
+		self.max_clients = max_clients
+		self.client_list = []
+		self.address_list = []
+		self.files_list = []
+
+	# Function for handling connections. This will be used to create threads
+	def client_thread(self, conn, addr):
+		stop_char = '|'
+		separation_char = '>'
+		try:
+			# Sending message to connected client
+			print("GG")
+
+			conn.sendall(('Welcome to the server.\n' + stop_char).encode('utf-8'))
+			print("G")
+			while True:
+				# Receiving from client
+				choice = ""
+				data = ""
+				while data != stop_char:
+					data = conn.recv(1).decode('utf-8')
+					if data == stop_char:
 						break
-				s=s+1
-				if found==1:
-					reply= 'Found desired file'
-					data=""
-					while data!='|':
-						data=conn.recv(1)
-						if data=='|':
-							break
-						choice=choice+data
-					if choice=='OK':
-						i.sendall('OK')
-						i.sendall('|')
-					else:
-						print 'Communication between clients failed'
-						i.sendall('fail')
-						i.sendall('|')
-					#print reply
-					break
-				reply='Did not found desired file'
-			conn.sendall(reply)
-			conn.sendall('|')
-			print reply
-			continue
-		
-	#came out of loop
-	conn.close()
+					choice += data
+				# reply = addr[0] + ':' + str(addr[1]) + ' sent: ' + choice + '\n'
+				print(addr[0], ':', str(addr[1]), 'sent:', choice, '\n')
 
-#now keep talking with the client
-while 1:
-    #wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    clist.append(conn)
-    flist.append([])
-    alist.append(addr)
-    
-    #print clist
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-     
-    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    start_new_thread(clientthread ,(conn,addr))
- 
-s.close()
+				# client disconnected
+				if choice == 'exit':
+					i = 0
+					reply = addr[0] + ':' + str(addr[1]) + ' disconnected from server.\n|'
+					for e in self.client_list:
+						if e == conn:
+							self.files_list[i] = []
+							self.files_list.remove([])
+							continue
+						e.sendall(reply.encode('utf-8'))
+						i += 1
+					self.client_list.remove(conn)
+					print(reply)
+					break
+	
+				# upload list of files
+				elif choice == 'up':
+					i = 0
+					# TO CHECK
+					for e in self.client_list:
+						if e == conn:
+							self.files_list[i] = []
+							word = ""
+							while word != stop_char:
+								data = ""
+								word = ""
+								while word != separation_char:
+									word = conn.recv(1).decode('utf-8')
+									if word == stop_char:
+										break
+									elif word == separation_char:
+										self.files_list[i].append(data)
+										break
+									data = data+word
+							print('File list has been updated:\n')
+							print(self.files_list)
+							break
+						i += 1
+					continue
+				# downloading list of files
+				elif choice == 'down':
+					reply = '\nFiles list:'
+					i = 0
+					for e in self.client_list:
+						if e == conn:
+							i += 1
+							continue
+						reply += '\nClient' + str(i+1) + ': '
+						for a in range(len(self.files_list[i])):
+							reply += self.files_list[i][a] + ' '
+						i += 1
+					conn.sendall((reply+stop_char).encode('utf-8'))
+					continue
+				elif choice == 'ask':
+					filename = ""
+					data = ""
+					# Receive filename to be sent
+					while data != stop_char:
+						data = conn.recv(1).decode('utf-8')
+						if data == stop_char:
+							break
+						filename += data
+					print('Searching for file ' + filename)
+					found = 0
+					i = 0
+					for e in self.client_list:
+						if e == conn:
+							i += 1
+							continue
+						for a in range(len(self.files_list[i])):
+							if filename == self.files_list[i][a]:
+								reply = 'OK'
+								conn.sendall((reply + stop_char).encode('utf-8'))
+								data = ""
+								port = ""  # probably this is for port
+								while data != stop_char:
+									data = conn.recv(1).decode('utf-8')
+									if data == stop_char:
+										break
+									port += data
+
+								e.sendall(('send' + stop_char + addr[0] + stop_char + port + stop_char + filename + stop_char).encode('utf-8'))
+								found = 1
+								break
+						i += 1
+						if found == 1:
+							reply = 'Found desired file'
+							data = ""
+							while data != stop_char:
+								data = conn.recv(1).decode('utf-8')
+								if data == stop_char:
+									break
+								choice += data
+							if choice == 'OK':
+								print("Transfer was successful")
+								# conn.sendall(('OK' + stop_char).encode('utf-8'))
+							else:
+								print('Communication between clients failed')
+								# conn.sendall(('fail' + stop_char).encode('utf-8'))
+							break
+						reply = 'Did not found desired file'
+					conn.sendall((reply+stop_char).encode('utf-8'))
+					print(reply)
+					continue
+	
+			conn.close()
+		except Exception as e:
+			print("Server's client_thread crashed:", e)
+
+	def run(self):
+
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		print('Socket created')
+
+		# Bind socket to local host and port
+		try:
+			s.bind((self.host, self.port))
+		except Exception as msg:
+			print('Bind failed. Error: ', str(msg))
+			sys.exit()
+		print('Socket bind complete')
+		# Start listening on socket
+		s.listen(self.max_clients)
+		print('Socket now listening')
+
+		# now keep talking with the client
+		while 1:
+			# wait to accept a connection - blocking call
+			conn, addr = s.accept()
+			self.client_list.append(conn)
+			self.files_list.append([])
+			self.address_list.append(addr)
+
+			# print client_list
+			print('Connected with ' + addr[0] + ':' + str(addr[1]))
+
+			# start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+			t = threading.Thread(target=self.client_thread, args=(conn, addr))
+			t.start()
+
+		s.close()
+
+
+server = Server()
+server.run()
